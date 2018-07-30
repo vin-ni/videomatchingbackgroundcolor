37a9bfa2dbc10504dc5644e9d27a10f992665821
Vinzenz Aubry <vinzenzaubry@gmail.com>
2018-07-30 14:58:23 +0200

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# videomatchingbackgroundcolor
-Embed html videos with correct background color
+# Matching HTML5 videos to a background color
+This is an example on how to handle the color derivation bug concerning html5 videos that should be inserted seamlessly into a website. I use this mostly for big videos that integrate into the landing page. The full explanation on how it works can be read [here](https://sansho.studio/blog/html-videos-correct-background-color).
+
+![drivation bug](example.gif "example gif")
+
+Basically it renders the video onto a canvas and keeps it in sync with the hidden video. Simultaneously it picks up the background color of the video frame and adjusts the websites color.
+
+The original solution comes from [Feng](https://stackoverflow.com/questions/35214962/html5-video-background-color-not-matching-background-color-of-website-in-some#44523649). I added retina support and a fallback for browsers with blocked autoplay (thanks safari mobile).
+
+Feel free to improve :)
---
+++ src/videoCenter/js/main.js
@@ -0,0 +1,102 @@
+/* eslint-disable */
+//  Embed html videos with correct background color
+//  Created by Vinzenz Aubry for sansho
+//  Feel free to improve!
+//  Base Code by Feng (https://stackoverflow.com/questions/35214962/html5-video-background-color-not-matching-background-color-of-website-in-some#44523649)
+//	Contact: vinzenz@sansho.studio
+
+var vid = document.getElementById('exampleVideo');
+
+var wrapper = document.getElementById('wrapper');
+var canvas = document.getElementById('exampleCanvas');
+var ctx = canvas.getContext('2d');
+
+var ratio = window.devicePixelRatio || 1;
+var vidWidth;
+var vidHeight;
+
+
+vid.addEventListener('loadedmetadata', function(e){
+	console.log('onloadedmetadata');
+	vid.style.transform = `scale(${ratio})`;
+
+	vidWidth = vid.videoWidth;
+	vidHeight = vid.videoHeight;
+
+	canvas.width = vid.offsetWidth * ratio;
+	canvas.height = vid.offsetHeight * ratio;
+
+	canvas.style.width = vid.offsetWidth + "px";
+	canvas.style.height = vid.offsetHeight + "px";
+
+	drawingLoop();
+});
+
+vid.addEventListener('loadeddata', function(e){
+	console.log('onloadeddata');
+	setVideoBgColor(vid, wrapper);
+	vid.play();
+});
+
+function drawingLoop() {
+	requestId = window.requestAnimationFrame(drawingLoop)
+
+	ctx.drawImage(vid, 0, 0, vidWidth, vidHeight, // source rectangle
+		0, 0, canvas.width, canvas.height); // destination rectangle);
+}
+
+function setVideoBgColor(vid, backgroundElement) {
+	// draw first four pixel of video to a canvas
+	// then get pixel color from that canvas
+	var canvas = document.createElement("canvas");
+	canvas.width = 8;
+	canvas.height = 8;
+
+	var ctx = canvas.getContext("2d");
+	ctx.drawImage(vid, 0, 0, 8, 8);
+
+	var p = ctx.getImageData(0, 0, 8, 8).data;
+	//dont take the first but fourth pixel [r g b]
+	backgroundElement.style.backgroundColor = "rgb(" + p[60] + "," + p[61] + "," + p[62] + ")";
+}
+
+window.onresize = function (event) {
+	vidWidth = vid.videoWidth;
+	vidHeight = vid.videoHeight;
+
+	canvas.width = vid.offsetWidth * ratio;
+	canvas.height = vid.offsetHeight * ratio;
+
+	canvas.style.width = vid.offsetWidth + "px";
+	canvas.style.height = vid.offsetHeight + "px";
+
+	//redraw canvas after resize
+	ctx.drawImage(vid, 0, 0, vidWidth, vidHeight, // source rectangle
+		0, 0, canvas.width, canvas.height); // destination rectangle);
+};
+
+
+//add src
+vid.src = vid.dataset.src;
+vid.play();
+
+(function () {
+	var fixAutoPlay = function (e) {
+
+		if(vid.paused){
+			vid.play();
+		}
+
+		// Remove events
+		document.removeEventListener('touchstart', fixAutoPlay);
+		document.removeEventListener('touchend', fixAutoPlay);
+		document.removeEventListener('scroll', fixAutoPlay);
+	};
+	// iOS 6-8
+	document.addEventListener('touchstart', fixAutoPlay);
+	// iOS 9
+	document.addEventListener('touchend', fixAutoPlay);
+	// Scroll (Just in case for Desktop)
+	document.addEventListener('scroll', fixAutoPlay);
+
+})();
